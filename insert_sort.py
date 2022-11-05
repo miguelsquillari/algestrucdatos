1cf872ad68d3a969016e91786b068a6e3db47889
msquillari <miguel.squillari@gmail.com>
2022-11-05 15:21:35 -0300

    algoritmo insertion sort

--- insert_sort.py
+++ insert_sort.py
@@ -4,16 +4,21 @@ arr = [4,5,2,5,7,8,910,200]
 
 print(arr)
 
-for j in range(1, len(arr)):
-    actual = arr[j]
-    print("valor actual", actual, " Posicion ", j)
-    i = j-1
-    print("condicion: valor de i:", i, " valor arr[i]:", arr[i] , " Valor de actual:", actual)
-    os.system('cls')
-    while i >= 0 and arr[i] > actual:
-        arr[i+1] = arr[i]
-        i -=1
+def insert_sort():
+    for j in range(1, len(arr)):
+        actual = arr[j]
+        print("valor actual", actual, " Posicion ", j)
+        i = j-1
+        print("condicion: valor de i:", i, " valor arr[i]:", arr[i] , " Valor de actual:", actual)
+        os.system('cls')
+        while i >= 0 and arr[i] > actual:
+            arr[i+1] = arr[i]
+            i -=1
     arr[i+1] = actual
     print("posicion", i+1 , " actual ", actual)
 
 print(arr)
+
+
+def call_insert():
+    pass
